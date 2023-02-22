c00e6ff9df3fd464dd30a961dd39677ecb4749aa
coder <122115043+tiffanysfw1@users.noreply.github.com>
2023-02-22 08:40:48 +0300

    Update path.c

--- path.c
+++ path.c
@@ -1,29 +1,25 @@
-
-
 #include "shell.h"
-
 /**
 * find_path - finds the path from the global enviroment
 * Return: NULL if path is not found or path if path is found.
 */
 char *find_path(void)
 {
-	int x;
-	char **env = environ, *path = NULL;
-
-	while (*env)
-	{
-		if (_strncmp(*env, "PATH=", 5) == 0)
-		{
-			path = *env;
-			while (*path && x < 5)
-			{
-				path++;
-				x++;
-			}
-			return (path);
-		}
-		env++;
-	}
-	return (NULL);
+int x;
+char **env = environ, *path = NULL;
+while (*env)
+{
+if (_strncmp(*env, "PATH=", 5) == 0)
+{
+path = *env;
+while (*path && x < 5)
+{
+path++;
+x++;
+}
+return (path);
+}
+env++;
+}
+return (NULL);
 }
